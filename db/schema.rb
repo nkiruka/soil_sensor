3a73cbdbde39b5d8531d985c7c96b04038e8c0e1
nkiru onwuneme <generalhrh@gmail.com>
2018-01-25 19:54:34 -0800

    add sensors table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180126034837) do
+ActiveRecord::Schema.define(version: 20180126035203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20180126034837) do
   create_table "plants", force: :cascade do |t|
     t.string "name"
     t.integer "threshold"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sensors", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
+    t.string "model"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
