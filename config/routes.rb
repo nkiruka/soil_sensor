5237e7eb7ab5602375b64e4c617622e019fe1557
nkiru onwuneme <generalhrh@gmail.com>
2018-01-25 22:24:59 -0800

    styled dashboard and added route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root to: 'home#index'
+
+  get 'dashboard', to: 'home#index'
+
   resources :sensors
   resources :readings
   resources :plants
+
 end
