0e1236ee70779ed87ce75992626c1fc512693fd1
nkiru onwuneme <generalhrh@gmail.com>
2018-01-26 11:31:26 -0800

    modified seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Sensor.create!([
-  {name: "Sensor 1", description: "moisture ", model: "Y-161",plant_id: 1},
-  {name: "Sensor 2", description: "moisture", model: "Y-161",plant_id: 2},
-  {name: "Sensor 3", description: "moisture", model: "Y-161", plant_id: 3},
-  {name: "Sensor 4", description: "moisture", model: "Y-161", plant_id: 4},
+Plant.create!([
+  {name: "Spider Plant", threshold: 80},
+  {name: "English Ivy", threshold: 60},
+  {name: "Heather Plant", threshold: 40},
+  {name: "Kalachoe", threshold: 30}
+  ])
 
+Sensor.create!([
+  {name: "Sensor 1", description: "moisture ", model: "Y-161",plant_id: plant.first},
+  {name: "Sensor 2", description: "moisture", model: "Y-161",plant_id: plant.find(2)},
+  {name: "Sensor 3", description: "moisture", model: "Y-161", plant_id: plant.find(3)},
+  {name: "Sensor 4", description: "moisture", model: "Y-161", plant_id: plant.first}
 ])
+
+sensor1 = Sensor.first
+sensor2 = Sensor.find(2)
+sensor3 = Sensor.find(3)
+sensor4 = Sensor.last
 
 Reading.create!([
   {sensor_id: 1,value: 50, plant_id: 1, created_at: Time.now},
@@ -37,10 +48,3 @@ Reading.create!([
   {sensor_id: 3, value:30, plant_id: 3,created_at: Time.now},
   {sensor_id: 4, value: 29, plant_id: 4,created_at: Time.now+2}
 ])
-
-Plant.create!([
-  {name: "Spider Plant", threshold: 80},
-  {name: "English Ivy", threshold: 60},
-  {name: "Heather Plant", threshold: 40},
-  {name: "Kalachoe", threshold: 30}
-  ])
